997b711996230093b603f983907917e31dc09642
Lukasz Oleksy <oleksy.lukasz@gmail.com>
2016-11-06 16:06:18 +0100

    modify: js localforage as replacement for largelocalstorage

--- src/AppBundle/Resources/public/js/core/Storage.js
+++ src/AppBundle/Resources/public/js/core/Storage.js
@@ -2,36 +2,19 @@ var myGpsWorkouts = myGpsWorkouts || {};
 myGpsWorkouts.core = myGpsWorkouts.core || {};
 
 myGpsWorkouts.core.Storage = function(){
-    var desiredCapacity = 256 * 1024 * 1024;
-    this.storageBackend = new LargeLocalStorage({size: desiredCapacity, name: 'myGpsWorkoutsDb'});
-    this.storageInitialized = false;
-    var that = this;
-    this.storageBackend.initialized.then(function() {
-        if(that.storageBackend.getCapacity() > -1){
-            that.storageInitialized = true;
-        }
+    this.storageBackend = localforage.createInstance({
+        name: "'myGpsWorkoutsDb"
     });
 };
 
 myGpsWorkouts.core.Storage.prototype.getTrackpoints  = function(workoutId, callback){
-    if(!this.storageInitialized){
-        callback(null);
-    }
-    this.storageBackend.getContents('trackpoints_' + String(workoutId)).then(function(content) {
-        if(content){
-            callback(JSON.parse(content));
-        }
-        else{
-            callback(null);
-        }
+    this.storageBackend.getItem('trackpoints_' + String(workoutId)).then(function(value) {
+        callback(value);
     });
 };
 
 myGpsWorkouts.core.Storage.prototype.setTrackpoints  = function(workoutId, data, callback){
-    if(!this.storageInitialized){
-        callback(null);
-    }
-    this.storageBackend.setContents('trackpoints_' + String(workoutId), JSON.stringify(data)).then(function() {
+    this.storageBackend.setItem('trackpoints_' + String(workoutId), data).then(function () {
         callback();
-    });
+    })
 };
